d3dc36b84675789802265dd2ff31cfc01f6e244e
Chaemin-153 <0520032@naver.com>
2021-03-02 15:03:16 +0900

    #2.6 Complete

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-const express = require("express");
+import express from "express";
+//  =const express = require("express");
+
 const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
-  console.log(`Listening on: http://localhost:${PORT}`);
-}
+// This is arrow function
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send('Hello from home :)');
+
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+// middleware needs 'next'
+const betweenHome = (req, res, next) => {
+  console.log("Between");
+  next();
+};
+
+// middleware's order is important
+
+app.use(betweenHome);   // This is 전역
+
+app.get("/", handleHome);
+
+//  This is 부분적용
+//  => app.get("/", betweenHome, handleHome);
+
+app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);
