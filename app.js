5fa553b0ba9879b347fd7c886e1b30665f51881c
Chaemin-153 <0520032@naver.com>
2021-03-05 15:07:09 +0900

    #2.17 Complete

--- app.js
+++ app.js
@@ -8,14 +8,17 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
+import { localMiddleWare } from "./middlewares";
 const app = express();
 
+app.use(helmet());
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(helmet());
 app.use(morgan("dev"));
+
+app.use(localMiddleWare);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
