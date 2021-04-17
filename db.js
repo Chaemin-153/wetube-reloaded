e9bdf1f0bed1ecf83473e2459402255b219465fd
Chaemin-153 <0520032@naver.com>
2021-04-18 07:04:18 +0900

    #4.5 Complete

--- db.js
+++ db.js
@@ -6,7 +6,9 @@ mongoose.connect(
   process.env.MONGO_URL,
   {
     useNewUrlParser: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true
   }
 );
 
