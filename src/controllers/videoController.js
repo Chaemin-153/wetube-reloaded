84bd2dc45e6c8a2cac9a17070c1056bc3349fd9b
Chaemin-153 <0520032@naver.com>
2021-06-09 00:07:18 +0900

    #6.18 Complete

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,9 @@
-import Video from "../models/Videos";
+import Video from "../models/Video";
 
-export const home = (req, res) => {
-    Video.find({}, (error, videos) => { });
-    return res.render("home", { pageTitle: "Home" });
+export const home = async (req, res) => {
+    const videos = await Video.find({});
+    console.log(videos);
+    return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = (req, res) => {
     const { id } = req.params;
@@ -22,8 +23,19 @@ export const getUpload = (req, res) => {
     return res.render("upload", { pageTitle: "Upload Video" });
 };
 
-export const postUpload = (req, res) => {
-    const { title } = req.body;
-    videos.push(newVideo);
-    return res.redirect("/");
+export const postUpload = async (req, res) => {
+    const { title, description, hashtags } = req.body;
+    try {
+        await Video.create({
+            title,
+            description,
+            hashtags: hashtags.split(",").map((word) => `#${word}`)
+        });
+        return res.redirect("/");
+    } catch (error) {
+        return res.render("upload", {
+            pageTitle: "Upload Video",
+            errorMessage: error._message
+        });
+    }
 };
