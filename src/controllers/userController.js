4db99119e3c9eae8420c71c4287448f8e684d476
Chaemin-153 <0520032@naver.com>
2021-06-22 20:29:14 +0900

    #7.23 Complete

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -44,7 +44,7 @@ export const getLogin = (req, res) =>
 export const postLogin = async (req, res) => {
     const { username, password } = req.body;
     const pageTitle = "Login";
-    const user = await User.findOne({ username });
+    const user = await User.findOne({ username, socialOnlu: false });
     if (!user) {
         return res.status(400).render("login", {
             pageTitle,
@@ -114,32 +114,33 @@ export const finishGithubLogin = async (req, res) => {
             (email) => email.primary === true && email.verified === true
         );
         if (!emailObj) {
+            // set notification
             return res.redirect("/login");
         }
-        const existingUser = await User.findOne({ email: emailObj.email });
-        if (existingUser) {
-            req.session.loggedIn = true;
-            req.session.user = existingUser;
-            return res.redirect("/");
-        } else {
-            const user = await User.create({
-                name: userData.name ? userData.name : "Unknown",
+        let user = await User.findOne({ email: emailObj.email });
+        if (!user) {
+            user = await User.create({
+                avatarUrl: userData.avatar_url,
+                name: userData.name,
                 username: userData.login,
                 email: emailObj.email,
                 password: "",
                 socialOnly: true,
                 location: userData.location
             });
-            req.session.loggedIn = true;
-            req.session.user = user;
-            return res.redirect("/");
         }
+        req.session.loggedIn = true;
+        req.session.user = user;
+        return res.redirect("/");
     } else {
         return res.redirect("/login");
     }
 };
 
+export const logout = (req, res) => {
+    req.session.destroy();
+    return res.redirect("/");
+};
+
 export const edit = (req, res) => res.send("Edit User");
-export const remove = (req, res) => res.send("Remove User");
-export const logout = (req, res) => res.send("Logout");
 export const see = (req, res) => res.send("See User");
--- src/models/User.js
+++ src/models/User.js
@@ -2,6 +2,7 @@ import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
+    avatarUrl: String,
     socialOnly: { type: Boolean, default: false },
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
