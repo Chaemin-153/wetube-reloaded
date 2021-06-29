3aaa46a15b36006f490b6993e03ee56ef3f6090b
Chaemin-153 <0520032@naver.com>
2021-06-29 18:58:42 +0900

    #9.0 Complete

--- src/models/User.js
+++ src/models/User.js
@@ -9,12 +9,15 @@ const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String },
     location: String,
+    videos: [
+        { type: mongoose.Schema.Types.ObjectId, ref: "Video" }
+    ],
 });
 
 userSchema.pre('save', async function () {
-    console.log("Users password: ", this.password);
-    this.password = await bcrypt.hash(this.password, 5);
-    console.log("Hashed password: ", this.password);
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 5);
+    }
 });
 
 const User = mongoose.model("User", userSchema);
