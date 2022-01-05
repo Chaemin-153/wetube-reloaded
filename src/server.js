52ff1801b5cadd86ae32b66f901f91e3d4a76d13
Chaemin-153 <0520032@naver.com>
2022-01-05 22:19:30 +0900

    #4.5 Architecture Recap

--- src/server.js
+++ src/server.js
@@ -1,40 +1,21 @@
 import express from "express";
 import morgan from "morgan";
-import session from "express-session";
-import flash from "express-flash";
-import MongoStore from "connect-mongo";
-import rootRouter from "./routers/rootRouter";
+import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
-import apiRouter from "./routers/apiRouter";
-import { localsMiddleware } from "./middlewares";
+
+const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-
-app.set("view engine", "pug");
-app.set("views", process.cwd() + "/src/views");
 app.use(logger);
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(
-    session({
-        secret: process.env.COOKIE_SECRET,
-        resave: false,
-        saveUninitialized: false,
-        store: MongoStore.create({
-            mongoUrl: process.env.DB_URL
-        })
-    })
-);
 
-app.use(flash());
-app.use(localsMiddleware);
-app.use("/uploads", express.static("uploads"));
-app.use("/static", express.static("assets"), express.static("node_modules/@ffmpeg/core/dist"));
-app.use("/", rootRouter);
+app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
-app.use("/api", apiRouter);
 
-export default app;
+const handleListening = () => {
+	console.log(`✅ Server listening on port http://localhost:${PORT}`);
+};
+
+app.listen(PORT, handleListening);
