c68f1899264af5653e30df34a8c52ceaddafc879
Chaemin-153 <0520032@naver.com>
2022-01-06 11:32:01 +0900

    #5.10 Recap

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,38 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const watch = (req, res) => res.send("Watch");
-export const edit = (req, res) => res.send("Edit");
+export const trending = (req, res) => {
+	const videos = [
+		{
+			title: "First Video",
+			rating: 5,
+			comments: 2,
+			createdAt: "2 minutes ago",
+			views: 59,
+			id: 1,
+		},
+		{
+			title: "Second Video",
+			rating: 5,
+			comments: 2,
+			createdAt: "2 minutes ago",
+			views: 59,
+			id: 1,
+		},
+		{
+			title: "Third Video",
+			rating: 5,
+			comments: 2,
+			createdAt: "2 minutes ago",
+			views: 59,
+			id: 1,
+		},
+	];
+	return res.render("home", { pageTitle: "Home", videos });
+};
+export const see = (req, res) => {
+	return res.render("watch", { pageTitle: "Watch" });
+};
+export const edit = (req, res) => {
+	return res.render("edit", { pageTitle: "Edit" });
+};
+export const search = (req, res) => res.send("Search");
+export const upload = (req, res) => res.send("Upload");
+export const deleteVideo = (req, res) => res.send("Delete Video");
